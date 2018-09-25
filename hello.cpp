55a496e2951c5057a8844112226239aba1c514bb
quangliem <phamquangliem96@gmail.com>
2018-09-25 21:28:24 +0700

    thử xem được không

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+/* C Example */
+#include <bits/stdc++.h>
+using namespace std;
+// #include <mpi.h>
+
+
+// int main (argc, argv)
+//      int argc;
+//      char *argv[];
+// {
+//   int rank, size;
+
+//   MPI_Init (&argc, &argv);	/* starts MPI */
+//   MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* get current process id */
+//   MPI_Comm_size (MPI_COMM_WORLD, &size);	/* get number of processes */
+//   printf( "Hello world from process %d of %d\n", rank + 1, size );
+//   MPI_Finalize();
+//   return 0;
+// }
+int main(){
+	function<int (int, int)> add = [&](int u, int v){
+		return u + v;
+	};
+	cout << add(1,2);
+	return 0;
+}
